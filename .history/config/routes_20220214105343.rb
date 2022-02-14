2342be4459c0592467e90532cc4a061c11a37f46
Cedric <cedricmarcelino1105@gmail.com>
2022-02-14 10:57:39 +0800

    Added tailwind, devise and category model.

--- .history/config/routes_20220214105343.rb
+++ .history/config/routes_20220214105343.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  resources :users do
-    resources :categories
-  end
+  resources :users
+  resources :categories
   devise_for :users
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
